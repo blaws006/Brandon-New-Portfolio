672751e11842abbb84deba07aa7686678042ce53
blaws006 <blaws006>
2018-01-24 22:28:18 -0500

    contact info button active

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,50 @@
-$(document).ready(function(){
+$(document).ready(function () {
   console.log("Connected")
   $('.parallax').parallax();
-  
-   var options = [
-      {selector: '#staggered-test', offset: 400, callback: function(el) {
-        Materialize.showStaggeredList($(el));
-      } }
-    ];
-  
-   Materialize.scrollFire(options);
 
-   $('.carousel.carousel-slider').carousel({
-     fullWidth: true,
-     indicators: true
-   });
   
+
+
+  var options = [{
+    selector: '#staggered-test',
+    offset: 400,
+    callback: function (el) {
+      Materialize.showStaggeredList($(el));
+    }
+  }];
+
+  Materialize.scrollFire(options);
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+
   $('#port-btn').on('click', function () {
-     
+
     $('.carousel.carousel-slider').carousel('next');
   });
 
-   $('#port-btn').on('touchstart', function (event) {
-     event.preventDefault();
-     $('.carousel.carousel-slider').carousel('next');
-   });
-});
+  //Smooth Scroll
+  function scrollNav() {
+    $("#nav a").not('#not').click(function () {
+      //Toggle Class
+      $(".active").removeClass("active");
+      $(this).closest('li').addClass("active");
+      var theClass = $(this).attr("class");
+      $('.' + theClass).parent('li').addClass('active');
+      //Animate
+      $('html, body').stop().animate({
+        scrollTop: $($(this).attr('href')).offset().top - 0
+      }, 800);
+      return false;
+    });
+    $('.scrollTop a').scrollTop();
+  }
+  scrollNav();
 
+  //Open and closing the target
+  
+
+ 
+});
